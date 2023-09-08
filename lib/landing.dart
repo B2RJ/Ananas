2f7371446c093e824295188f9ca1d3076d9ee3be
B2RJ <bruneau.rich@gmail.com>
2023-09-08 21:00:13 +0200

    Update footer

--- lib/landing.dart
+++ lib/landing.dart
@@ -3,9 +3,6 @@
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
 
-// 📦 Package imports:
-import 'package:url_launcher/url_launcher.dart';
-
 // 🌎 Project imports:
 import 'homepage.dart';
 
@@ -16,78 +13,65 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(children: [
-        // The main content of the page
-        Expanded(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const AnimatedOpacity(
-                  duration: Duration(milliseconds: 500),
-                  opacity: 1,
-                  child: Text(
-                    'Welcome to Session',
-                    style: TextStyle(
-                        fontSize: 32, color: Colors.white, fontFamily: 'Arial'),
+      body: Column(
+        children: [
+          // The main content of the page
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const AnimatedOpacity(
+                    duration: Duration(milliseconds: 500),
+                    opacity: 1,
+                    child: Text(
+                      'Welcome to Session',
+                      style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.white,
+                          fontFamily: 'Arial'),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                AnimatedOpacity(
-                  duration: const Duration(milliseconds: 500),
-                  opacity: 1,
-                  child: TextButton(
-                    onPressed: () => Navigator.push(
-                      context,
-                      PageRouteBuilder(
-                        transitionDuration: const Duration(seconds: 1),
-                        pageBuilder: (context, animation, secondaryAnimation) {
-                          return FadeTransition(
-                            opacity: animation,
-                            child: const HomePage(),
-                          );
-                        },
+                  const SizedBox(height: 16),
+                  AnimatedOpacity(
+                    duration: const Duration(milliseconds: 500),
+                    opacity: 1,
+                    child: TextButton(
+                      onPressed: () => Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          transitionDuration: const Duration(seconds: 1),
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) {
+                            return FadeTransition(
+                              opacity: animation,
+                              child: const HomePage(),
+                            );
+                          },
+                        ),
+                      ),
+                      child: const Text(
+                        'start',
+                        style: TextStyle(color: Colors.greenAccent),
                       ),
                     ),
-                    child: const Text(
-                      'start',
-                      style: TextStyle(color: Colors.greenAccent),
-                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        // The footer with the "Made with love in YYC" text
-        Container(
+          // The footer with the "Made with love in YYC" text
+          Container(
             height: 90,
             alignment: Alignment.center,
-            child: GestureDetector(
-              onTap: () {
-                _launchURL(); // Fonction pour ouvrir le lien lorsque le texte est cliqué
-              },
-              child: const Text(
-                'Développé avec amour par Richoulebg. ♡',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.blue,
-                  decoration: TextDecoration.underline,
-                  fontFamily: 'Arial',
-                ),
-              ),
-            ))
-      ]),
+            child: const Text(
+              'Built in YYC by Jash Dubal. Improved by B2RJ.',
+              style: TextStyle(
+                  fontSize: 12, color: Colors.white24, fontFamily: 'Arial'),
+            ),
+          ),
+        ],
+      ),
     );
-  }
-
-  // Fonction pour ouvrir le lien dans le navigateur
-  _launchURL() async {
-    var uri = Uri.http('paypal.me', '/RichardBruneau');
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri);
-    } else {
-      throw 'Impossible d\'ouvrir l\'URL : $uri';
-    }
   }
 }
