acdbe6b5363572b69f9c973f8440a5ddfb635c3a
B2RJ <48026144+B2RJ@users.noreply.github.com>
2023-09-26 19:39:22 +0200

    improve test (#1)

--- lib/pages/data.dart
+++ lib/pages/data.dart
@@ -78,154 +78,156 @@ class _DataState extends State<Data> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: GridView(
-                    shrinkWrap: true,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 2,
-                      mainAxisSpacing: 0,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: GridView(
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 2,
+                        mainAxisSpacing: 0,
+                      ),
+                      children: [
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          color: Colors.black,
+                          shadowColor: Colors.greenAccent,
+                          elevation: 15,
+                          child: Padding(
+                            padding: const EdgeInsets.all(26.0),
+                            child: Column(
+                              children: [
+                                const Icon(
+                                  Icons.access_time_filled_outlined,
+                                  color: Colors.greenAccent,
+                                  size: 50,
+                                ),
+                                Text(_totalMin.toString(),
+                                    style: const TextStyle(
+                                        color: Colors.greenAccent,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Arial')),
+                                const Text('total minutes',
+                                    style: TextStyle(
+                                        color: Colors.greenAccent,
+                                        fontSize: 12,
+                                        fontFamily: 'Arial',
+                                        fontStyle: FontStyle.italic)),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          color: Colors.black,
+                          shadowColor: Colors.greenAccent,
+                          elevation: 15,
+                          child: Padding(
+                            padding: const EdgeInsets.all(26.0),
+                            child: Column(
+                              children: [
+                                const Icon(
+                                  Icons.emoji_events,
+                                  color: Colors.greenAccent,
+                                  size: 50,
+                                ),
+                                Text(_longestSesh.toString(),
+                                    style: const TextStyle(
+                                        color: Colors.greenAccent,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Arial')),
+                                const Text('longest session',
+                                    style: TextStyle(
+                                        color: Colors.greenAccent,
+                                        fontSize: 12,
+                                        fontFamily: 'Arial',
+                                        fontStyle: FontStyle.italic)),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          color: Colors.black,
+                          shadowColor: Colors.greenAccent,
+                          elevation: 15,
+                          child: Padding(
+                            padding: const EdgeInsets.all(26.0),
+                            child: Column(
+                              children: [
+                                const Icon(
+                                  Icons.calendar_month_rounded,
+                                  color: Colors.greenAccent,
+                                  size: 50,
+                                ),
+                                Text(_seshNum.toString(),
+                                    style: const TextStyle(
+                                        color: Colors.greenAccent,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Arial')),
+                                const Text('number of sessions',
+                                    style: TextStyle(
+                                        color: Colors.greenAccent,
+                                        fontSize: 12,
+                                        fontFamily: 'Arial',
+                                        fontStyle: FontStyle.italic)),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          color: Colors.black,
+                          shadowColor: Colors.greenAccent,
+                          elevation: 15,
+                          child: Padding(
+                            padding: const EdgeInsets.all(26.0),
+                            child: Column(
+                              children: [
+                                const Icon(
+                                  Icons.bar_chart_rounded,
+                                  color: Colors.greenAccent,
+                                  size: 50,
+                                ),
+                                Text(
+                                    double.parse((_avgSesh).toStringAsFixed(2))
+                                        .toString(),
+                                    style: const TextStyle(
+                                        color: Colors.greenAccent,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Arial')),
+                                const Text('avg session time',
+                                    style: TextStyle(
+                                        color: Colors.greenAccent,
+                                        fontSize: 12,
+                                        fontFamily: 'Arial',
+                                        fontStyle: FontStyle.italic)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    children: [
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.black,
-                        shadowColor: Colors.greenAccent,
-                        elevation: 15,
-                        child: Padding(
-                          padding: const EdgeInsets.all(26.0),
-                          child: Column(
-                            children: [
-                              const Icon(
-                                Icons.access_time_filled_outlined,
-                                color: Colors.greenAccent,
-                                size: 50,
-                              ),
-                              Text(_totalMin.toString(),
-                                  style: const TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Arial')),
-                              const Text('total minutes',
-                                  style: TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontSize: 12,
-                                      fontFamily: 'Arial',
-                                      fontStyle: FontStyle.italic)),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.black,
-                        shadowColor: Colors.greenAccent,
-                        elevation: 15,
-                        child: Padding(
-                          padding: const EdgeInsets.all(26.0),
-                          child: Column(
-                            children: [
-                              const Icon(
-                                Icons.emoji_events,
-                                color: Colors.greenAccent,
-                                size: 50,
-                              ),
-                              Text(_longestSesh.toString(),
-                                  style: const TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Arial')),
-                              const Text('longest session',
-                                  style: TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontSize: 12,
-                                      fontFamily: 'Arial',
-                                      fontStyle: FontStyle.italic)),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.black,
-                        shadowColor: Colors.greenAccent,
-                        elevation: 15,
-                        child: Padding(
-                          padding: const EdgeInsets.all(26.0),
-                          child: Column(
-                            children: [
-                              const Icon(
-                                Icons.calendar_month_rounded,
-                                color: Colors.greenAccent,
-                                size: 50,
-                              ),
-                              Text(_seshNum.toString(),
-                                  style: const TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Arial')),
-                              const Text('number of sessions',
-                                  style: TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontSize: 12,
-                                      fontFamily: 'Arial',
-                                      fontStyle: FontStyle.italic)),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.black,
-                        shadowColor: Colors.greenAccent,
-                        elevation: 15,
-                        child: Padding(
-                          padding: const EdgeInsets.all(26.0),
-                          child: Column(
-                            children: [
-                              const Icon(
-                                Icons.bar_chart_rounded,
-                                color: Colors.greenAccent,
-                                size: 50,
-                              ),
-                              Text(
-                                  double.parse((_avgSesh).toStringAsFixed(2))
-                                      .toString(),
-                                  style: const TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Arial')),
-                              const Text('avg session time',
-                                  style: TextStyle(
-                                      color: Colors.greenAccent,
-                                      fontSize: 12,
-                                      fontFamily: 'Arial',
-                                      fontStyle: FontStyle.italic)),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
                   ),
-                ),
+                )
               ],
             )));
   }
